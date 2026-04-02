f0235115ede68fd87d862911072a9041a7921b37
Joshan Randhawa <joshanrandhawa5@hotmail.com>
2026-04-02 18:48:58 +0100

    Secondary thoughts on 27

--- 27/27.py
+++ 27/27.py
@@ -1,8 +1,12 @@
-#remove the given value from the the array using the "in place algorithm" to make a new array
-#Then return the amount of inputs within our new nums that are remaining without val
-#Make the variable of whats remaining be called K and then print K
+#in place is the name for multiple types of sorts that share the same memory as one another,
+#meaning I can use an insertion sort
+
+#make the array only hold the values of "val" this could technically also mean to just read all occurances of val or to delte everything but val in the array and then count the amount of occurances (K) that are within the array
+
+#print/return K
 #Do not worry about size
 
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        
