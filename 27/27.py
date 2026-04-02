786105925f1b8c604b67c52f98cc7e16e838f4dc
Joshan Randhawa <joshanrandhawa5@hotmail.com>
2026-04-02 18:48:24 +0100

    Add inital 27 thoughts

---
+++ 27/27.py
@@ -0,0 +1,8 @@
+#remove the given value from the the array using the "in place algorithm" to make a new array
+#Then return the amount of inputs within our new nums that are remaining without val
+#Make the variable of whats remaining be called K and then print K
+#Do not worry about size
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
