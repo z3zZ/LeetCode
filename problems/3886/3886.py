25a8cd97106a89e0c0f7e35f8803b64dd350966e
Joshan Randhawa <joshanrandhawa5@hotmail.com>
2026-04-05 00:37:44 +0100

    Add inital thoughts for 146 and 3886

---
+++ problems/3886/3886.py
@@ -0,0 +1,20 @@
+#find what n is
+#k = sortable if n/k
+#array = nums
+#array is sorted in a non-decreasing by partioning the array
+#equal partions of the length of k
+#hashmaps due to constraints
+
+
+
+@lc app=leetcode id=3886 lang=python3
+#
+# [3886] Sum of Sortable Integers
+#
+
+# @lc code=start
+class Solution:
+    def sortableIntegers(self, nums: list[int]) -> int:
+        
+# @lc code=end
+
